a1b7f30bb9651ae8483b1559275614195a46d7e6
Alexsbs002 <79782875+Alexsbs002@users.noreply.github.com>
2023-12-07 21:09:39 +0600

    Flutter lab

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,19 +1,27 @@
+// Импорт библиотек Flutter для построения пользовательских интерфейсов
 import 'package:flutter/material.dart';
+
+// Импорт пользовательских виджетов, представляющих контейнеры
 import 'first_container.dart';
 import 'second_container.dart';
 import 'third_container.dart';
 import 'fourth_container.dart';
 
+// Класс HomeScreen представляет главный экран приложения
 class HomeScreen extends StatelessWidget {
+  // Конструктор класса HomeScreen
   HomeScreen();
 
-
+  // Метод build строит и возвращает иерархию виджетов для главного экрана
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
+      // Использование виджета Scaffold для базовой структуры экрана
       body: SingleChildScrollView(
+        // Использование SingleChildScrollView для прокрутки содержимого экрана
         child: Column(
           children: [
+            // Вставка пользовательских контейнеров на главный экран
             FirstContainer(),
             SecondContainer(),
             ThirdContainer(),
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
 import 'home_screen.dart';
 
+
 void main() {
   runApp(MyApp());
 }
 
+// Основной класс приложения, который является виджетом без состояния (StatelessWidget)
 class MyApp extends StatelessWidget {
+  // Конструктор класса MyApp
   MyApp({Key? key});
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // Установка темы приложения, включая цвет фона и схему цветов
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        scaffoldBackgroundColor: Colors.white, // Установка цвета фона экрана
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple), // Установка схемы цветов
+        useMaterial3: true, // Использование Material3, если доступно
       ),
+      // Установка главного экрана приложения в виде экземпляра HomeScreen
       home: HomeScreen(),
     );
   }
